6d7838932c6df70bd0f833f8bebf778a29e96e01
Acostita <acostasebas40@gmail.com>
2025-02-27 21:09:31 -0500

    adding configurations

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Amplify } from "aws-amplify";
 
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import outputs from "../../amplify_outputs.json";
+import outputs from "@/amplify_outputs.json";
 
 Amplify.configure(outputs);
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Amplify } from "aws-amplify";
-import outputs from "../../amplify_outputs.json";
+import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 
